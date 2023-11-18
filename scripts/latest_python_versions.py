544e55007ea9274c83270688d3489837aefeebba
Josh Peak <neozenith.dev@gmail.com>
2023-11-19 04:45:10 +1100

    update python script to determine latest pyenv patched versions of python

--- scripts/latest_python_versions.py
+++ scripts/latest_python_versions.py
@@ -1,15 +1,37 @@
 import subprocess
-import dataclasses
+from pathlib import Path
 from itertools import groupby
 
-result = subprocess.run("pyenv install --list", capture_output=True, shell=True, text=True)
-tidy_versions = {
-    pyversion.strip(): tuple([int(d) for d in pyversion.strip().split(".")])  
-    for pyversion in result.stdout.strip().split("\n") 
-    if pyversion.strip().startswith("3.") and "dev" not in pyversion and all([d.isdigit() for d in pyversion.strip().split(".")])
-}
-print(tidy_versions)
+def update_pyenv():
+    """Update with latest information."""
+    PYENV_HOME=Path.home() / ".pyenv"
+    subprocess.run("git pull", capture_output=True, shell=True, text=True, cwd=PYENV_HOME)
 
-grouped_versions = groupby(tidy_versions.items(), lambda k, v: v[1] )
-max_versions = [max(g, key=lambda x: x) for k, g in grouped_versions]
-print(max_versions)
+def tidy_versions():
+    # Get all available versions
+    result = subprocess.run("pyenv install --list", capture_output=True, shell=True, text=True)
+
+    # Get all python versions that are only X.Y.Z format and only digits. No dev or alpha versions etc
+    return {
+        pyversion.strip(): tuple([int(d) for d in pyversion.strip().split(".")])  
+        for pyversion in result.stdout.strip().split("\n") 
+        if pyversion.strip().startswith("3.") and all([d.isdigit() for d in pyversion.strip().split(".")])
+    }
+
+def max_patched_versions():
+    # Group tuples by the minor version number
+    grouped_versions = groupby(tidy_versions().values(), lambda v: v[1] )
+    # Tuple sort to get the largest within each group
+    return [
+        '.'.join([str(n) for n in list(v)]) # Convert tuple back to dot formatted string
+        for v in sorted([
+            max(g, key=lambda x: x) 
+            for k, g in grouped_versions
+        ], reverse=True)
+    ]
+
+
+if __name__ == "__main__":
+    update_pyenv()
+    print("\n".join([f"pyenv install {v}" for v in max_patched_versions()[:3]]))
+    
